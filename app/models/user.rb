704e07b9f809b6e8ecb8c5327c1b73992967a31e
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-09 16:58:10 -0300

    Add the user model with following the diagram, and adding a name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :foods
+  has_many :recipes
+
+  validates :name, presence: true
 end

0dff38349659bccffb807dd4e4695cea0c9fdf0d
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-09 22:29:47 -0300

    Add the delete route to foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :foods, only: [:index, :new, :create]
+  resources :foods, only: [:index, :new, :create, :destroy]
 
   root 'foods#index'
 

96e76110aa506d822ac81b257f1b8041640eafc2
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-09 22:25:59 -0300

    Add numericaly validation for price and quantity

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,6 +5,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
   validates :measurement_unit, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 end

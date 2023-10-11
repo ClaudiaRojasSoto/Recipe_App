572318990f437c1f21eaac7192795089fcaeae74
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-11 09:55:36 -0300

    Add puts to depuring the logic

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,7 +9,9 @@ class Recipe < ApplicationRecord
 
     foods.each do |food|
       recipe_food = RecipeFood.find_by(recipe_id: id, food_id: food.id)
-      food_total = (recipe_food.quantity.to_i * food.price.to_i)
+      food_total = (recipe_food.quantity * food.price)
+      puts "--------------------------------------"
+      puts "recipe: #{self.name}, food: #{food.name}, quantity: #{recipe_food.quantity}, price: #{food.price}"
       prices.push(food_total)
     end
     prices.sum

7035224dab8634377a17657f6e97d42e95682c2f
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-11 01:08:59 -0300

    Add scope for the is delete function

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,8 @@ class Food < ApplicationRecord
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
 
+  scope :active, -> { where(is_deleted: false) }
+
   validates :name, presence: true
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }

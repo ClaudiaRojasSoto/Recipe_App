45ca651768b15035eaea6adf2f98403afdef1fe9
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-11 01:08:12 -0300

    Add logic to the destroy and create methods to avoid issues with the creation of foods with differents names, letters and others

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,9 +4,9 @@ class FoodsController < ApplicationController
 
   def index
     @foods = if params[:sort] == 'name'
-               current_user.foods.order('LOWER(name)')
+               current_user.foods.where(is_deleted: false).order('LOWER(name)')
              else
-               current_user.foods
+               current_user.foods.where(is_deleted: false)
              end
   end
 
@@ -15,20 +15,55 @@ class FoodsController < ApplicationController
   end
 
   def create
-    @food = current_user.foods.build(food_params)
-    if @food.save
-      redirect_to foods_path, notice: 'Food was successfully created.'
+    formatted_name = format_food_name(food_params[:name])
+    existing_food = current_user.foods.find_by(name: formatted_name)
+
+    puts "Existing food: #{existing_food.inspect}"
+
+    if existing_food
+      puts "is_deleted: #{existing_food.is_deleted}"
+
+      if existing_food.is_deleted == true
+        success = existing_food.update(
+          is_deleted: false,
+          quantity: food_params[:quantity].to_i,
+          price: food_params[:price].to_f
+        )
+        puts "Update when deleted was #{success ? 'successful' : 'unsuccessful'}"
+      else
+        # (para sumar la cantidad y actualizar el precio promedio)
+        new_quantity = existing_food.quantity + food_params[:quantity].to_i
+
+        # Calcula el precio promedio
+        total_cost_old = existing_food.price * existing_food.quantity
+        total_cost_new = food_params[:price].to_f * food_params[:quantity].to_i
+        total_cost = total_cost_old + total_cost_new
+        avg_price = total_cost / new_quantity
+
+        puts "new_quantity: #{new_quantity}"
+        puts "avg_price: #{avg_price}"
+
+        success = existing_food.update(quantity: new_quantity, price: avg_price)
+        puts "Update was #{success ? 'successful' : 'unsuccessful'}"
+      end
+
+      redirect_to foods_path, notice: 'Food quantity and price were successfully updated.'
     else
-      render :new
+      @food = current_user.foods.build(food_params.merge(name: formatted_name))
+      if @food.save
+        redirect_to foods_path, notice: 'Food was successfully created.'
+      else
+        render :new
+      end
     end
   end
 
   def destroy
     @food = Food.find(params[:id])
-    if @food.destroy
-      flash[:notice] = 'Food was successfully deleted.'
+    if @food.update(is_deleted: true)
+      flash[:notice] = 'Food was successfully "deleted".'
     else
-      flash[:alert] = 'Failed to delete food!'
+      flash[:alert] = 'Failed to "delete" food!'
     end
     redirect_to foods_path
   end
@@ -41,5 +76,10 @@ class FoodsController < ApplicationController
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+  def format_food_name(name)
+    formatted_name = name.strip.downcase.titleize.singularize
+    formatted_name.gsub(/\s+/, "")
   end
 end

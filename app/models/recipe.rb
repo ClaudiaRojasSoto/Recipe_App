311f243e51bd29fa8dd35d0c34cd12023f4ced20
Dachrono <andres_zamorano_@hotmail.com>
2023-10-12 11:30:00 -0600

    Add new validates for new recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,14 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+  #has_and_belongs_to_many :foods, join_table: 'recipe_foods'
   has_many :foods, through: :recipe_foods
   scope :public_recipes, -> { where(public: true).order(created_at: :desc) }
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 
   def total_foods_price
     prices = []
